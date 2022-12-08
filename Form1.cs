cdad14b121a42c9d2dc6471a9b933b3509151077
devdev59 <119765037+devdev59@users.noreply.github.com>
2022-12-08 21:29:31 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ozellik yeni = new ozellik();
+            // this.Text = yeni.mudur; // get blogu devreye girdi
+            yeni.mudur = "ahmet akın "; // set blogu devreye girdi
+            this.Text = yeni.mudur; 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ozellik yeni = new ozellik();
+            // this.Text = yeni.mudur;
+            yeni.mudur = "ahmet akın ";
+            yeni.butcemiktari = textBox1.Text;
+            this.Text = yeni.mudur + "'nın limiti " + yeni.butcemiktari + " dır."; 
+        }
+    }
+}
---
+++ ozellik.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp6
+{
+    internal class ozellik
+    {
+        private double miktar = 0;
+        private string isim = "yelda fırat";
+        public string mudur //properties
+        {
+            get
+            {
+              return isim;
+            }
+            set
+            {
+                this.isim = value;
+            }
+        }
+        public string butcemiktari
+        {
+            get 
+            {
+                return string.Format("{0:c}", miktar);
+            }
+            set
+            { 
+                this.miktar = Convert.ToDouble(value);
+            }
+        }
+    }
+}
